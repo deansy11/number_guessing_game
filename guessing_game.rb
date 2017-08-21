736eec7d7706bc08d9207e6552217e497a8394c0
Emily Deans <emilyldeans@gmail.com>
2017-08-21 15:36:24 -0400

    Created an if statement to check if user number = computer random number.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,8 +1,14 @@
 # this generates a random number between 1 and 100 (including 100, because it uses '..' instead of '...')
-rand(1..100)
+randNum = rand(1..100)
 
 # this allows you to receive input from a user
 print "Pick a random number Between 1 and 100. Please put your guess here:"
 
 # this creates a new variable and asks for user input (via 'gets'). 'to_i' converts the response to an integer and 'chomp' removes the addition of a new line from the end of the integer.
 here = gets.chomp.to_i
+
+# creates an if statement to check if user number = computer random number
+if here == randNum
+  puts "Nailed it!"
+else
+  puts "Whoops. Try again."
