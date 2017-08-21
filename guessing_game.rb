116f1eadb63f997868a9c18460fc666ad57c04fc
Emily Deans <emilyldeans@gmail.com>
2017-08-21 15:31:23 -0400

    Created variable to hold user input and convert to integer.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,3 +1,8 @@
+# this generates a random number between 1 and 100 (including 100, because it uses '..' instead of '...')
 rand(1..100)
 
-print "Guess a number between 1 and 100."
+# this allows you to receive input from a user
+print "Pick a random number Between 1 and 100. Please put your guess here:"
+
+# this creates a new variable and asks for user input (via 'gets'). 'to_i' converts the response to an integer and 'chomp' removes the addition of a new line from the end of the integer.
+here = gets.chomp.to_i
